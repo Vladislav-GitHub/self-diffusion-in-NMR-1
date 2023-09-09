63eec43ed8de11ac17e28027e3640e464af8e775
e.davletshin <e.davletshin@gmail.com>
2023-09-09 11:55:23 +0500

    typo fixes

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 import streamlit as st
 import requests
 
-# Добавить заголовок
-st.title('Bootcamp hackaton')
-
 # Заголовок приложения
 st.title('Автоматический подбор параметров самодиффузии по результатам ЯМР')
-
-# Адрес сервера для обработки файла
-url = 'http://host.docker.internal:8000/file_handler'
 
 # Вводимый пользователем файл
 uploaded_file = st.file_uploader("Загрузите файл с данными эксперимента", type=[])
@@ -22,15 +16,14 @@ if uploaded_file is not None:
         # Отправка файла на сервер FastAPI для обучения модели
         response = requests.post("http://host.docker.internal:8000/train/", files={"file": uploaded_file})
         
-
         try:
             response_data = response.json()
         except ValueError:
             response_data = response.text
 
         if ("fitted_A" and "fitted_B") in response_data:
-            st.write("Подобранные параметры 1: ", response_data["fitted_A"]) 
-            st.write("Подобранные параметры 2: ",response_data["fitted_B"])
+            st.write("Подобранные населенности: ", response_data["fitted_A"]) 
+            st.write("Подобранные коэффициента самодиффузии: ",response_data["fitted_B"])
         else:
             st.write("Не удалось подобрать оптимальные параметры")
         
@@ -41,8 +34,3 @@ if uploaded_file is not None:
             st.line_chart(loss_history)
         else:
             st.write("Loss history not found in the server response.")
-
-
-        
-
-        
